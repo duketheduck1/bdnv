e48fc2f65b9a22fbedcd5a843b76fb966269e313
eu6en <46784927+eu6en@users.noreply.github.com>
2025-05-16 09:57:59 -0400

    Fix: Make wallet auth work on localhost
    Use the client_id that's located at Privy Dashboard -> BDNV -> App Settings -> Clients -> Local Dev Environment

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -8,6 +8,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
       config={{
         loginMethods: ['wallet'],
       }}
